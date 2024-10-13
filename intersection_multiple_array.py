ef78f832785409a96d500e45dd9360eb0faf638f
Lawrence Gandhar <lawrence_gandhar@yahoo.com>
2024-10-13 23:59:17 +0530

    Update intersection_multiple_array.py

--- intersection_multiple_array.py
+++ intersection_multiple_array.py
@@ -38,12 +38,57 @@ class Solution:
         new_dict = {key: val for key,val in dict1.items() if val==xx}
         
         return sorted([key for key in new_dict.keys()])
+    
+    # using quick sort
+    def method2(self, nums):
+        dict1 = {}
+        xx = len(nums)
+        for i in range(xx):
+            for j in range(len(nums[i])):
+                if nums[i][j] in dict1:
+                    dict1[nums[i][j]] += 1
+                else:
+                    dict1.update({nums[i][j]:1})
+
+        new_list = [key for key, val in dict1.items() if val==xx]
+
+        if len(new_list) > 1: 
+            return self.quick_sort(new_list)
+        else:
+            return new_list
+
+    def quick_sort(self, list1, low=0, high=None):
+        if high is None:
+            high = len(list1) - 1
+
+        if low < high:
+            pivot = self.partition(list1, low, high)
+            self.quick_sort(list1, low, pivot - 1)
+            self.quick_sort(list1, pivot + 1, high)
+            
+        return list1
+
+    def partition(self, array, low, high):
+        i = low - 1
+        pivot = array[high]
+        for j in range(low, high):
+            if array[j] <= pivot:
+                i += 1
+                array[i], array[j] = array[j], array[i]
+        array[i+1], array[high] = array[high], array[i+1]
+        return i + 1
+
 
 t1 = [[3,1,2,4,5],[1,2,3,4],[3,4,5,6]]
 t2 = [[1,2,3],[4,5,6]]
 t3 = [[7,34,45,10,12,27,13],[27,21,45,10,12,13]]
+t4 = [[25,44,47,42,43,10],[40,10,8,30,5,23],[36,10]]
 
 ss = Solution()
-print(ss.intersection(t1))
-print(ss.intersection(t2))
-print(ss.intersection(t3))
+# print(ss.intersection(t1))
+# print(ss.intersection(t2))
+print(ss.method2(t3))
+print(ss.method2(t4))
+
+
+# ss.quick_sort([9, 45, 10, 12, 27, 13])
